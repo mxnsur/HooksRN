37c6d72f8501fcbbfdf7a165cb615c4ef73a43b2
“Rafael <rafaelmansursn@gmail.com>
2023-07-29 19:30:36 -0300

    estrelas funcionando rankeamento

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import Estrelas from "../../../componentes/Estrelas";
 
 export default function Produtor ({ nome, imagem, distancia, estrelas }) {
-    return <View style={estilos.cartao}>
+    const [ selecionado, setSelecionado ] = useState();
+
+    return <TouchableOpacity 
+        style={estilos.cartao}
+        onPress={() => setSelecionado(!selecionado)}
+    >
         <Image source={imagem} style={estilos.imagem} accessibilityLabel={nome} />
         <View style={estilos.informacoes}>
             <View>
                 <Text style={estilos.nome}>{ nome } </Text>
-                <Estrelas quantidade={estrelas} />
+                <Estrelas 
+                quantidade={estrelas} 
+                editavel={selecionado}
+                grande={selecionado}
+                />
             </View>
              <Text style={estilos.distancia}>{ distancia } </Text>
 
         </View>
-        </View>
+        </TouchableOpacity>
 }
 
 const estilos = StyleSheet.create({
