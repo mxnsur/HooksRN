27b2ab7b0eb8b4fdd844d3d178c5f49fb4d2aabb
“Rafael <rafaelmansursn@gmail.com>
2023-07-29 01:16:46 -0300

    inserindouseEffect

--- src/telas/Home/componentes/Produtores.js
+++ src/telas/Home/componentes/Produtores.js
@@ -1,14 +1,35 @@
-import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, StyleSheet } from 'react-native';
 import { carregaProdutores } from '../../../servicos/carregaDados';
 
 
 export default function Produtores(){
+    const [titulo, setTitulo] = useState('');
+    const [lista, setLista] = useState([]);
 
     useEffect(() => {
         const retorno = carregaProdutores();
-        console.log(retorno);
+        setTitulo(retorno.titulo);
+        setLista(retorno.lista);
+        //console.log(retorno);
     }, []);
 
-    return <Text>Produtores</Text>
+    const TopoLista = () => {
+        return <Text>{ titulo }</Text>
+    }
+    return <FlatList
+          data={lista} 
+          renderItem={({item: {nome}}) => <Text>{ nome }</Text>}
+          ListHeaderComponent={TopoLista}/>
+        
 }
+    const estilos = StyleSheet.create({
+        titulo: {
+            fontSize: 20,
+            lineHeight: 32,
+            marginHorizontal: 16,
+            marginTop: 16,
+            fontWeight: 'bold'
+        }
+
+    })
