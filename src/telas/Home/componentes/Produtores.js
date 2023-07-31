d4fedf3c2e52b260570e987fa6a7f2558b0a4c73
“Rafael <rafaelmansursn@gmail.com>
2023-07-31 15:21:23 -0300

    criando um Hook - set list e set titulo

--- src/telas/Home/componentes/Produtores.js
+++ src/telas/Home/componentes/Produtores.js
@@ -2,40 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
 
 import Produtor from './Produtor';
-import { carregaProdutores } from '../../../servicos/carregaDados';
+import useProdutoress from '../../../hooks/useProdutoress';
 
 export default function Produtores({ topo: Topo}) {
-    const [titulo, setTitulo] = useState('');
-    const [lista, setLista] = useState([]);
-
-    useEffect(() => {
-        const retorno = carregaProdutores();
-        setTitulo(retorno.titulo);
-        setLista(retorno.lista);
-        //console.log(retorno);
-    }, []);
+    const [titulo, lista] = useProdutoress();
 
     const TopoLista = () => {
         return <>
-        <Topo />
-        <Text style={estilos.titulo}>{ titulo }</Text>
+            <Topo />
+            <Text style={estilos.titulo}>{ titulo }</Text>
         </>
     }
-    return <FlatList
-          data={lista} 
-          renderItem={({ item }) => <Produtor {...item} />}
-          keyExtractor={({nome}) => nome}
-          ListHeaderComponent={TopoLista}/>
-        
-}
-    const estilos = StyleSheet.create({
-        titulo: {
-            fontSize: 20,
-            lineHeight: 32,
-            marginHorizontal: 16,
-            marginTop: 16,
-            fontWeight: 'bold',
-            color: "#464646"
-        }
 
-    })
+    return <FlatList 
+        data={lista}
+        renderItem={({ item }) => <Produtor {...item} />}
+        keyExtractor={({nome}) => nome}
+        ListHeaderComponent={TopoLista} />
+}
+
+const estilos = StyleSheet.create({
+    titulo: {
+        fontSize: 20,
+        lineHeight: 32,
+        marginHorizontal: 16,
+        marginTop: 16,
+        fontWeight: 'bold',
+        color: '#464646',
+    }
+})
